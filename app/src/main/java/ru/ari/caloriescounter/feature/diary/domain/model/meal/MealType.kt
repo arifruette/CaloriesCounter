ae46cb71fbe978282c57c1d9723b9d1f75c8a4dd
Amir Khazeev <amirhazeev60189@gmail.com>
2026-03-17 22:39:17 +0300

    Добавлен ужин как прием пищи

--- app/src/main/java/ru/ari/caloriescounter/feature/diary/domain/model/meal/MealType.kt
+++ app/src/main/java/ru/ari/caloriescounter/feature/diary/domain/model/meal/MealType.kt
@@ -3,5 +3,6 @@ package ru.ari.caloriescounter.feature.diary.domain.model.meal
 enum class MealType {
     BREAKFAST,
     LUNCH,
+    DINNER,
     SNACK,
 }
--- app/src/main/java/ru/ari/caloriescounter/feature/diary/presentation/common/MealTypeTitleRes.kt
+++ app/src/main/java/ru/ari/caloriescounter/feature/diary/presentation/common/MealTypeTitleRes.kt
@@ -8,5 +8,6 @@ import ru.ari.caloriescounter.feature.diary.domain.model.meal.MealType
 fun MealType.titleRes(): Int = when (this) {
     MealType.BREAKFAST -> R.string.meal_breakfast
     MealType.LUNCH -> R.string.meal_lunch
+    MealType.DINNER -> R.string.meal_dinner
     MealType.SNACK -> R.string.meal_snack
 }
